e94d03d3d627e86ffc6b3f6e5bb70a3dcec17b80
yrtik <yrtik@192.168.0.16>
2021-02-17 19:21:40 +0900

    コンストラクタとゲッター作成

--- javaPracticeChat/src/model/User.java
+++ javaPracticeChat/src/model/User.java
@@ -3,6 +3,17 @@ package model;
 import java.io.Serializable;
 
 public class User implements Serializable {
-
+	private String name;
+	private String pass;
+	public User(String name, String pass) {
+		this.name = name;
+		this.pass = pass;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getPass() {
+		return pass;
+	}
 
 }
